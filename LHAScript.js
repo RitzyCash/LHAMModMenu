7caaa0ae1a06e3b97868e6cd12c2a0061408b1c4
RitzyCash <104687977+RitzyCash@users.noreply.github.com>
2026-02-14 12:19:33 +0000

    Update LHAScript.js

--- LHAScript.js
+++ LHAScript.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  QoL improvements for LHA School Synergy, including custom backgrounds, text customization, and image redirection.
-// @author       Antigravity
+// @author       SwiftSeal
 // @match        https://lha.schoolsynergy.co.uk/*
 // @grant        GM_addStyle
 // @grant        GM_setValue
